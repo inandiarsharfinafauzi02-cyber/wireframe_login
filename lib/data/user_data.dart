db091b0d565ac7dd4810e2f755128419a41cbaa3
inandiarsharfinafauzi <inandiarsharfinafauzi02@gmail.com>
2025-09-15 18:49:04 +0700

    komit pertama

---
+++ lib/data/user_data.dart
@@ -0,0 +1,7 @@
+Map<String, Map<String, String>> userData = {
+  // Contoh data awal (bisa dikosongkan)
+  'test@email.com': {
+    'fullName': 'Test User',
+    'password': 'password123',
+  },
+};
